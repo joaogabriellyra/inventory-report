4b26630de40a7271f3b102515d67a22394dc5d46
João Gabriel Lyra <jgabriellyra@hotmail.com>
2023-03-16 20:02:03 -0300

    corrigindo erros

--- inventory_report/reports/simple_report.py
+++ inventory_report/reports/simple_report.py
@@ -12,12 +12,12 @@ class SimpleReport:
         for item in list:
             if item["data_de_fabricacao"] < data_fabricacao:
                 data_fabricacao = item["data_de_fabricacao"]
-            if data_hoje <= item["data_de_validade"] < data_fabricacao:
+            if data_hoje <= item["data_de_validade"] < data_validade:
                 data_validade = item["data_de_validade"]
             nomes_empresas.append(item["nome_da_empresa"])
-        return (f"""
-        Data de fabricação mais antiga: {data_fabricacao}
-        Data de validade mais próxima: {data_validade}
-        Empresa com mais produtos: {Counter(nomes_empresas)
-        .most_common(1)[0][0]}
-        """)
+        empresa = Counter(nomes_empresas).most_common(1)[0][0]
+        return (
+            f"Data de fabricação mais antiga: {data_fabricacao}\n"
+            f"Data de validade mais próxima: {data_validade}\n"
+            f"Empresa com mais produtos: {empresa}"
+        )
