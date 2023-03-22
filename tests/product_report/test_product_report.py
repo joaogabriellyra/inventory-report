49564f5102ba507707e63f18e90d8a7796786b76
João Gabriel Lyra <jgabriellyra@hotmail.com>
2023-03-22 18:43:54 -0300

    requisito 8 concluído

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,5 +1,12 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_relatorio_produto():
-    pass  # Seu teste deve ser escrito aqui
+    mock1 = "O produto RAÇÃO fabricado em 2022-04-04 por Agroneto com validade"
+    mock2 = " até 2023-02-09 precisa ser armazenado Conservar em local fresco."
+    produto = Product(
+        1, "RAÇÃO", "Agroneto", "2022-04-04", "2023-02-09",
+        "FR48", "Conservar em local fresco"
+    )
+
+    assert produto.__repr__() == mock1 + mock2
