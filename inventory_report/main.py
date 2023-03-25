dedff6aef71ad55c826a122759d7f24c5d1b5905
João Gabriel Lyra <jgabriellyra@hotmail.com>
2023-03-25 13:36:40 -0300

    erro do avaliador na máquina

--- inventory_report/main.py
+++ inventory_report/main.py
@@ -1,2 +1,13 @@
+from inventory_report.inventory.inventory import Inventory
+import sys
+
+
 def main():
-    pass
+    try:
+        path = sys.argv[1]
+        type = sys.argv[2]
+        inventory_report = Inventory()
+        print(inventory_report.import_data(path, type))
+    except IndexError as err:
+        err = "Verifique os argumentos\n"
+        return err
